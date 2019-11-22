35a992293713babb43438a7e157d0804c84acf44
t3rmin41 <t33rmin41@gmail.com>
2019-11-22 11:38:06 +0200

    Added edx.org problems package

---
+++ mainEdx.py
@@ -0,0 +1,9 @@
+from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles
+
+# Below are some lines of code that will test your function.
+# You can change the value of the variable(s) to test your
+# function with different inputs.
+#
+# If your function works correctly, this will originally
+# print: [1, 2, 3, 4, 5]
+print(sort_with_bubbles([5, 3, 1, 2, 4]))
