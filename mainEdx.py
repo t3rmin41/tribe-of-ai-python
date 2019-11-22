38b512fb5da405513628bf6724214837b85100c7
t3rmin41 <t33rmin41@gmail.com>
2019-11-22 15:07:18 +0200

    mergesort added

--- mainEdx.py
+++ mainEdx.py
@@ -1,4 +1,4 @@
-from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles, sort_with_select
+from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles, sort_with_select, mergesort
 
 # Below are some lines of code that will test your function.
 # You can change the value of the variable(s) to test your
@@ -15,3 +15,10 @@ print(sort_with_bubbles([5, 3, 1, 2, 4]))
 #If your function works correctly, this will originally
 #print: [1, 2, 3, 4, 5]
 print(sort_with_select([5, 3, 1, 2, 4]))
+
+#Let's try it out!
+print(mergesort([2, 5, 3, 8, 6, 9, 1, 4, 7]))
+
+#It works! To see more about how it works, check out
+#MergesortwithPrints.py. To get a succinct version of
+#this algorithm, checkout MergesortShort.py.
