a8079de6d50938199080616e84ef9f4cc305cc2a
t3rmin41 <t33rmin41@gmail.com>
2019-11-22 15:01:32 +0200

    edx: selection sort algorithm solved

--- mainEdx.py
+++ mainEdx.py
@@ -1,4 +1,4 @@
-from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles
+from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles, sort_with_select
 
 # Below are some lines of code that will test your function.
 # You can change the value of the variable(s) to test your
@@ -7,3 +7,11 @@ from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles
 # If your function works correctly, this will originally
 # print: [1, 2, 3, 4, 5]
 print(sort_with_bubbles([5, 3, 1, 2, 4]))
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print: [1, 2, 3, 4, 5]
+print(sort_with_select([5, 3, 1, 2, 4]))
