b77b6f9ef2fedb341fe3e53f473e88eab2bfbfd8
t3rmin41 <t33rmin41@gmail.com>
2019-11-29 15:49:06 +0200

    Edx exam problem 2

--- mainEdx.py
+++ mainEdx.py
@@ -1,5 +1,9 @@
 from tribe.ai.edx.SortingAlgorithms import sort_with_bubbles, sort_with_select, mergesort
+# rule of thumb for big O notation : the number of nested loops (nested!) on the same list is power of n
+# e.g. O(n^2) - there are 2 nested loops, O(n) [linear] - one nested loop. Or there can be several loops on the same list
+# but if loops are not nested - the complexity is O(n) [linear]
 from tribe.ai.edx.SearchAlgorithms import linear
+from tribe.ai.edx.Exam import *
 
 # Below are some lines of code that will test your function.
 # You can change the value of the variable(s) to test your
@@ -7,7 +11,7 @@ from tribe.ai.edx.SearchAlgorithms import linear
 #
 # If your function works correctly, this will originally
 # print: [1, 2, 3, 4, 5]
-print(sort_with_bubbles([5, 3, 1, 2, 4]))
+#print(sort_with_bubbles([5, 3, 1, 2, 4]))
 
 #Below are some lines of code that will test your function.
 #You can change the value of the variable(s) to test your
@@ -15,10 +19,10 @@ print(sort_with_bubbles([5, 3, 1, 2, 4]))
 #
 #If your function works correctly, this will originally
 #print: [1, 2, 3, 4, 5]
-print(sort_with_select([5, 3, 1, 2, 4]))
+#print(sort_with_select([5, 3, 1, 2, 4]))
 
 #Let's try it out!
-print(mergesort([2, 5, 3, 8, 6, 9, 1, 4, 7]))
+#print(mergesort([2, 5, 3, 8, 6, 9, 1, 4, 7]))
 
 #It works! To see more about how it works, check out
 #MergesortwithPrints.py. To get a succinct version of
@@ -32,4 +36,16 @@ print(mergesort([2, 5, 3, 8, 6, 9, 1, 4, 7]))
 #If your function works correctly, this will originally
 #print: 3
 a_list = [5, 1, 3, 7, 3, 1, 7, 8, 3, 6]
-print(linear(a_list, 6))
+#print(linear(a_list, 6))
+
+some_number = 17
+
+print("fibonacci(", some_number, ") : ", end = "")
+print(fibonacci(some_number))
+
+print("collatz(", some_number, ") : ", end = "")
+print(collatz(some_number))
+
+print("collatz2(", some_number, ") : ", end = "")
+print(collatz2(some_number))
+
---
+++ tribe/ai/edx/Exam.py
@@ -0,0 +1,24 @@
+def fibonacci(some_number):
+    if some_number <= 2 :
+        return 1
+    else:
+        return fibonacci(some_number - 1) + fibonacci(some_number - 2)
+
+def collatz(current_number):
+     print(current_number, " ", end = "")
+     if current_number % 2 == 0:
+         return collatz(current_number // 2)
+     else:
+         if current_number != 1:
+             return collatz(current_number * 3 + 1)
+
+def collatz2(current_number):
+    print(current_number, " ", end = "")
+    if current_number % 2 == 0:
+        return collatz(current_number // 2)
+    elif current_number % 2 == 1 :
+            return collatz(current_number * 3 + 1)
+    else :
+        #pass
+        return 1 # Though the correct answer - None of these will work with the remainder of the code as written.
+
