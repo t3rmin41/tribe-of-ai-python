f28d4b54b7c3239ee46e821fcf1c32887945fc34
t3rmin41 <t33rmin41@gmail.com>
2019-12-15 17:35:55 +0200

    RainsOfReason: alphabeticShift solved

--- modules/tribe/ai/intro/RainsOfReason.py
+++ modules/tribe/ai/intro/RainsOfReason.py
@@ -2,3 +2,14 @@ import re
 def variableName(name):
     pattern = r"^[a-zA-Z_][a-zA-Z_0-9]*"
     return bool(re.fullmatch(pattern, name))
+
+def alphabeticShift(inputString):
+    shifted_dict = dict()
+    ascii_in_number = range(65, 122)
+    for i in ascii_in_number:
+        shifted_dict[chr(i)] = chr(i+1)
+    shifted_dict['z'] = 'a'
+    outputString = []
+    for c in inputString:
+        outputString.append(shifted_dict.get(c))
+    return "".join(outputString)
